9e4319161a6158af83b77ad3c4c8bc7136a82734
kasheesh <kashishbhurani@gmail.com>
2024-07-19 20:40:03 +0530

    added cart icon

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FaCartShopping } from "react-icons/fa6";
 function Navbar({ productCount }){
   return (
     <div className = "bg-white flex justify-between">
     <img className = "w-24 py-2 ml-32"src = "https://logos-world.net/wp-content/uploads/2020/04/Amazon-Logo.png"/>
-      <span>{productCount}</span>
+    <div className='mr-12'>
+      <span className='text-xs'>{productCount}</span>
+      <FaCartShopping/>
+  
+    </div>
     </div>
   );
 }
